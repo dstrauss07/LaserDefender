5ae572fef7f34edd1956671bb38b0465ec7a126d
dstrauss <fmdenver@gmail.com>
2019-01-31 22:44:35 -0500

    1.31

--- LaserDefender/Assets/Scripts/EnemyPathing.cs
+++ LaserDefender/Assets/Scripts/EnemyPathing.cs
@@ -5,17 +5,55 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour
 {
 
-    [SerializeField] List<Transform> waypoints;
+    [SerializeField] WaveConfig waveConfig;
+    [SerializeField] List<Transform> waypoints;  //for debugging
+    [SerializeField] GameObject path;
+    [SerializeField] float moveSpeed = 2f;
+    int waypointIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        path = waveConfig.GetPathPrefab();
+        AddPath();
+        transform.position = waypoints[waypointIndex].position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards
+                (transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                Debug.Log("waypoint reached!" + waypointIndex);
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void AddPath()
+    {
+        //adding all path children in list pathWaypoints
+        for (int i = 0; i < path.transform.childCount; i++)
+        {
+            waypoints.Add(path.transform.GetChild(i));
+        }
+
+
     }
 }
